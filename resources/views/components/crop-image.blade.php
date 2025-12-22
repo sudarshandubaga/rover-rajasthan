65d97ef50cf6789ecdc9a38fafe15536f993d336
Sudarshan Dubaga <sudarshandubaga@gmail.com>
2025-12-22 13:37:54 +0530

    chore: Re-indent commented HTML blocks in the admin layout.

--- resources/views/components/crop-image.blade.php
+++ resources/views/components/crop-image.blade.php
@@ -23,3 +23,72 @@
         </div>
     </div>
 </div>
+
+@push('extra_scripts')
+    <script>
+        $(document).ready(function () {
+            $('#{{ $name }}_file').on('change', function (event) {
+                let file = event.target.files[0];
+                if (file) {
+                    let reader = new FileReader();
+                    reader.onload = function (e) {
+                        let imageFileId = event.target.id;
+                        let cropModal = $(`#crop-modal-{{ $name }}`);
+                        let cropImg = $(`#crop-img-{{ $name }}`);
+                        let previewContainer = $(`#preview-container-{{ $name }}`);
+                        let previewImage = $(`#preview-image-{{ $name }}`);
+                        let croppedImageInput = $(`#cropped-image-{{ $name }}`);
+                        let cropActions = $(`#crop-actions-{{ $name }}`);
+
+                        // Show the modal
+                        cropModal.show();
+                        cropImg.attr('src', e.target.result);
+
+                        // Initialize Cropper after image is loaded
+                        cropImg.on('load', function () {
+                            let cropper = new Cropper(cropImg[0], {
+                                aspectRatio: {{ $width }} /
+                                                            {{ $height }},
+                                viewMode: 1,
+                                autoCropArea: 1,
+                                crop(event) {
+                                    let canvas = cropper.getCroppedCanvas({
+                                        width: {{ $width }},
+                                        height: {{ $height }}
+                                                            });
+                                    previewImage.attr('src', canvas.toDataURL(
+                                        'image/webp'));
+                                }
+                            });
+
+                            // Save button: Update the textarea with the cropped image
+                            $(`#save-cropped-{{ $name }}`).on('click', function () {
+                                let canvas = cropper.getCroppedCanvas({
+                                    width: {{ $width }},
+                                    height: {{ $height }}
+                                                        });
+
+                                $(`#drop-zone-{{ $name }} img`).remove();
+                                $(`#drop-zone-{{ $name }}`).append(
+                                    `<img src="${canvas.toDataURL('image/webp')}" />`
+                                );
+
+                                croppedImageInput.val(canvas.toDataURL('image/webp'));
+                                previewImage.attr('src', canvas.toDataURL(
+                                    'image/webp'));
+                                cropModal.hide(); // Close the modal after saving
+                            });
+
+                            // Cancel button: Close the modal and reset everything
+                            $(`#cancel-cropped-{{ $name }}`).on('click', function () {
+                                cropper.destroy();
+                                cropModal.hide();
+                            });
+                        });
+                    };
+                    reader.readAsDataURL(file);
+                }
+            });
+        });
+    </script>
+@endpush
