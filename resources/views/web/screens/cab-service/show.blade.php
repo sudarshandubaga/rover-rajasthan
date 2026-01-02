c597089db950d82f44e069334e4b3d97a5e6eb47
Sudarshan Dubaga <sudarshandubaga@gmail.com>
2026-01-02 13:15:21 +0530

    regular_fare in cab service

--- resources/views/web/screens/cab-service/show.blade.php
+++ resources/views/web/screens/cab-service/show.blade.php
@@ -124,7 +124,12 @@
                             <tr class="hover:bg-slate-50">
                                 <td class="py-4 px-6 font-medium">{{ $cab->vehicle_type }}</td>
                                 <td class="py-4 px-6">{{ $cab->capacity }} Seater</td>
-                                <td class="py-4 px-6">₹{{ $cab->fare }}/km</td>
+                                <td class="py-4 px-6">
+                                    @if (parseFloat($cab->regular_fare) > $cab->fare)
+                                        <del class="text-xs text-gray-500">₹{{ $cab->regular_fare ?: $cab->fare }}/km</del>
+                                    @endif
+                                    <span class="text-lg font-bold">₹{{ $cab->fare }}/km</span>
+                                </td>
                             </tr>
                         @endforeach
                     </tbody>
